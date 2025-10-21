81ec07eb967c1b325bd57178cbc5b0b62c323b73
Alexander Mohr <alexdermohr@gmail.com>
2025-10-21 15:23:25 +0200

    Document inert toggle and drawer E2E tests

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,8 +1,22 @@
-<script>
+<script lang="ts">
   import '../app.css';
   import 'maplibre-gl/dist/maplibre-gl.css';
+  import '$lib/styles/tokens.css';
+  import { onMount } from 'svelte';
+  import { ensureInertPolyfill } from '$lib/utils/inert-polyfill';
+  import { setUAClasses } from '$lib/utils/ua-flags';
+  import { page } from '$app/stores';
+  import { get } from 'svelte/store';
 
-  export let data;
+  export let data: any;
+
+  onMount(() => {
+    setUAClasses();
+    // Toggle: ?noinert=1 schaltet Polyfill ab (Debug/Kompat)
+    const q = new URLSearchParams(get(page).url.search);
+    const disable = q.get('noinert') === '1' || (window as any).__NO_INERT__ === true;
+    if (!disable) ensureInertPolyfill();
+  });
 </script>
 
 <svelte:head>
